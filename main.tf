5258b062bf090bd98ba7069c7382ab8bf94a735f
Lloyd3312 <faromo.j@gmail.com>
2026-02-13 22:11:55 +0000

    deployment and terraform adjusted

--- main.tf
+++ main.tf
@@ -71,10 +71,10 @@ module "eks" {
   
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.micro"]  
+      instance_types = ["t3.small"]  
       min_size       = 1
-      max_size       = 3
-      desired_size   = 3
+      max_size       = 2
+      desired_size   = 2
    
     }
   }
